8017c7929078bdd008a9eda4b12ea833d0bc9b97
‘peter-robert-cole’ <petercole3@gmail.com>
2019-12-06 16:49:28 -0800

    added gitignore

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+})
