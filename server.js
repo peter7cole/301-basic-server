0322606fa864556be412971a208e2f667aa4389c
‘peter-robert-cole’ <petercole3@gmail.com>
2019-12-06 17:21:19 -0800

    set up new portfolio on server

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+app.use(express.static('./public'));
 const PORT = process.env.PORT;
+
+// app.get('/', (request, response) => {
+//   response.send('<h1 onClick="window.location.reload();">Hello from the backside<h1>');
+// })
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
